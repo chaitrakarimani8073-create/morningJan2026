dd33877829c4c7d9f3f4ad4ef7ff305f44abbee3
Chaitra Tester <chaitrakarimani8073@gmail.com>
2026-02-02 13:56:07 +0530

    excel file is changed

--- src/users/TestScripts.java
+++ src/users/TestScripts.java
@@ -1,4 +1,23 @@
-package testng;
+package users;
 
-public class users {
-}
+import org.testng.annotations.Test;
+
+    public class TestScripts {
+
+        @Test
+        public void TS_TestScript1(){
+            System.out.println("The TS_TestScript1 executed Successful");
+        }
+
+        @Test
+        public void TS_TestScript2(){
+            System.out.println("The TS_TestScript2 executed Successful");
+        }
+
+        @Test
+        public void TS_TestScript3(){
+            System.out.println("The TS_TestScript3 executed Successful");
+        }
+    }
+
+
